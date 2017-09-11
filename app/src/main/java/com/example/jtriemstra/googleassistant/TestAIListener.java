74d8904e83789171483ef3471a7eb86d047aa225
Joel Triemstra <JTriemstra@jtriemstraw7.VML.LAN>
2017-09-11 18:00:12 -0400

    starting to add code from instructions, but compile error

---
+++ app/src/main/java/com/example/jtriemstra/googleassistant/TestAIListener.java
@@ -0,0 +1,36 @@
+package com.example.jtriemstra.googleassistant;
+
+import ai.api.*;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+
+
+/**
+ * Created by JTriemstra on 9/11/2017.
+ */
+public class TestAIListener implements AIListener {
+    public void onResult(AIResponse result)
+    {
+
+    }
+    public void onError(AIError error)
+    {
+
+    }
+    public void onAudioLevel(float level)
+    {
+
+    }
+    public void onListeningStarted()
+    {
+
+    }
+    public void onListeningCanceled()
+    {
+
+    }
+    public void onListeningFinished()
+    {
+
+    }
+}
