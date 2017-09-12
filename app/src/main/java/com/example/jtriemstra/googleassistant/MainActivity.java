b9ab9a8efe184d515fdc0db3f1bf24553af36503
Joel Triemstra <JTriemstra@jtriemstraw7.VML.LAN>
2017-09-11 21:05:33 -0400

    functional

--- app/src/main/java/com/example/jtriemstra/googleassistant/MainActivity.java
+++ app/src/main/java/com/example/jtriemstra/googleassistant/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.jtriemstra.googleassistant;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
 
 public class MainActivity extends Activity {
 
+    AIService aiService;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final AIConfiguration config = new AIConfiguration("348dec206c964356bbea514136328194",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(this, config);
+
+        aiService.setListener(new TestAIListener());
+    }
+
+
+
+    public void startClick(View view)
+    {
+        aiService.startListening();
+    }
+
+    public void stopClick(View view)
+    {
+        aiService.stopListening();
     }
 }
--- app/src/main/java/com/example/jtriemstra/googleassistant/TestAIListener.java
+++ app/src/main/java/com/example/jtriemstra/googleassistant/TestAIListener.java
@@ -1,5 +1,7 @@
 package com.example.jtriemstra.googleassistant;
 
+import android.util.Log;
+
 import ai.api.*;
 import ai.api.model.AIError;
 import ai.api.model.AIResponse;
@@ -9,28 +11,31 @@ import ai.api.model.AIResponse;
  * Created by JTriemstra on 9/11/2017.
  */
 public class TestAIListener implements AIListener {
+    private static final String TAG = "TestAIListener";
+
     public void onResult(AIResponse result)
     {
-
+        Log.d(TAG, "onResult");
+        Log.d(TAG, result.getResult().getFulfillment().getSpeech());
     }
     public void onError(AIError error)
     {
-
+        Log.d(TAG, "onError");
     }
     public void onAudioLevel(float level)
     {
-
+        Log.d(TAG, "onAudioLevel");
     }
     public void onListeningStarted()
     {
-
+        Log.d(TAG, "onListeningStarted");
     }
     public void onListeningCanceled()
     {
-
+        Log.d(TAG, "onListeningCanceled");
     }
     public void onListeningFinished()
     {
-
+        Log.d(TAG, "onListeningFinished");
     }
 }
